792db6b67e6782bb26035eae40493a3f1d89c9fd
Ankith <ankithamachoor2003@gmail.com>
2023-02-27 23:20:07 +0530

    Changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:app/Menu.dart';
 import 'package:flutter/material.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,76 +8,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int myIndex=0;
+  int myIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-backgroundColor: Colors.black,
-
+      backgroundColor: Colors.black,
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.shifting,
-        onTap: (index){
-          setState(() {
-
-          });
-        myIndex=index;
+        onTap: (index) {
+          setState(() {});
+          myIndex = index;
         },
-       currentIndex: myIndex,
-        items:const[
-        BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      label: 'Home',
-        backgroundColor: Colors.black),
-
-   BottomNavigationBarItem(
-       icon: Icon(Icons.search),
-       label: 'Search',
-   backgroundColor: Colors.black),
-        BottomNavigationBarItem(
-            icon:Icon(Icons.add),
-            label:'Add',
-        backgroundColor: Colors.black),
-        BottomNavigationBarItem(icon: Icon(Icons.notifications),
-        label: 'Notification',
-        backgroundColor: Colors.black),
-        BottomNavigationBarItem(icon: Icon(Icons.person),
-        label: 'Profile',
-        backgroundColor: Colors.black),
-      ],
+        currentIndex: myIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Add',
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: 'Notification',
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+              backgroundColor: Colors.black),
+        ],
       ),
-appBar: new AppBar(
-  backgroundColor: Colors.blue,
-  title: new Center(
-    child: new Text("Translator"),
-
-  ),
-),
-drawer: Drawer(
-  child: new ListView(
-    children: <Widget>[
-      new UserAccountsDrawerHeader(
-          accountName:new Text("M2K"), 
-          accountEmail:new Text("Translator"),
-      currentAccountPicture: new CircleAvatar(),),
- new ListTile(
-  title: new Text("Menu"),
-
-)
-      ,ListTile(
-        title: Text("Languages"),
+      appBar: new AppBar(
+        backgroundColor: Colors.blue,
+        title: new Center(
+          child: new Text("Translator"),
+        ),
       ),
-      ListTile(
-        title: Text("Settings"),
+      drawer: Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text("M2K"),
+              accountEmail: new Text("Translator"),
+              currentAccountPicture: new CircleAvatar(),
+            ),
+            new ListTile(
+              title: new Text("Menu"),
+            ),
+            ListTile(
+              title: Text("Languages"),
+            ),
+            ListTile(
+              title: Text("Settings"),
+            ),
+            ListTile(
+              title: Text("Exit"),
+            )
+          ],
+        ),
       ),
-      ListTile(
-        title: Text("Exit"),
-      )
-
-    ],
-  ),
-),
-
-      );
-
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+import 'dart:ui';
+
 void main() {
   runApp(const MyApp());
 }
 
-
-class MyApp extends StatelessWidget
-{
-  const MyApp({Key?key}):super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext){
+  Widget build(BuildContext) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
